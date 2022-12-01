49c7efadf50406e97b824e4247dc932692a8cef4
mahdiAhmedsharifa <118634010+mahdiAhmedsharifa@users.noreply.github.com>
2022-12-01 23:02:43 +0300

    Update convert_to_postman_spec.sh
    SELECT COUNT(Professor_Name) FROM SALARIES WHERE Salary > 150000;

--- convert_to_postman_spec.sh
+++ convert_to_postman_spec.sh
@@ -1,6 +1,1 @@
-rm -rf ./*.json
-
-for file in api-specifications/*
-do
-  openapi2postmanv2 -s "$file" -o "$(basename -- $file)" -p -O folderStrategy=Tags,requestParametersResolution=Schema,responseParametersResolution=Schema,optimizeConversion=false,stackLimit=50
-done
+SELECT DEPARTMENT, SUM(Salary) FROM SALARIES GROUP BY DEPARTMENT;SELECT COUNT(Professor_Name) FROM SALARIES WHERE Salary > 150000;
