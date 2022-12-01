3a7c99cf0886f03a01a55e0fc9949d3f985f06ad
mahdiAhmedsharifa <118634010+mahdiAhmedsharifa@users.noreply.github.com>
2022-12-01 23:02:07 +0300

    Update convert_to_postman_spec.sh

--- convert_to_postman_spec.sh
+++ convert_to_postman_spec.sh
@@ -1,6 +1,1 @@
-rm -rf ./*.json
 
-for file in api-specifications/*
-do
-  openapi2postmanv2 -s "$file" -o "$(basename -- $file)" -p -O folderStrategy=Tags,requestParametersResolution=Schema,responseParametersResolution=Schema,optimizeConversion=false,stackLimit=50
-done
